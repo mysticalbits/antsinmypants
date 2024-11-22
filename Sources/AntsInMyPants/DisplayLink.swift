33de3ff4e637860706d8b59d68b30046c03c61c3
kushagra666@gmail.com <kushsolitary@gmail.com>
2024-11-22 18:19:45 +0530

    make it compatible with macOS 12.0

--- Sources/AntsInMyPants/DisplayLink.swift
+++ Sources/AntsInMyPants/DisplayLink.swift
@@ -1,5 +1,66 @@
 import SwiftUI
 
+#if os(macOS)
+public class DisplayLink: ObservableObject {
+    private var displayLink: CVDisplayLink?
+    private var tickHandler: (_ framesPerSecond: CFTimeInterval) -> Void = {_ in}
+    private var previousTimestamp: CFTimeInterval?
+    
+    deinit {
+        stop()
+    }
+    
+    public func start(tickHandler: @escaping (_ framesPerSecond: CFTimeInterval) -> Void) {
+        self.tickHandler = tickHandler
+        previousTimestamp = nil
+        
+        var displayLink: CVDisplayLink?
+        CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+        
+        guard let displayLink = displayLink else {
+            return
+        }
+        
+        let opaqueself = Unmanaged.passUnretained(self).toOpaque()
+        
+        CVDisplayLinkSetOutputCallback(displayLink, { (displayLink, inNow, inOutputTime, flagsIn, flagsOut, opaque) -> CVReturn in
+            let myself = Unmanaged<DisplayLink>.fromOpaque(opaque!).takeUnretainedValue()
+            myself.tick(timestamp: inNow.pointee.timeInterval)
+            return kCVReturnSuccess
+        }, opaqueself)
+        
+        self.displayLink = displayLink
+        CVDisplayLinkStart(displayLink)
+    }
+    
+    public func stop() {
+        guard let displayLink = displayLink else { return }
+        CVDisplayLinkStop(displayLink)
+        self.displayLink = nil
+        previousTimestamp = nil
+    }
+    
+    private func tick(timestamp: CFTimeInterval) {
+        if let previousTimestamp = previousTimestamp {
+            let deltaTime = timestamp - previousTimestamp
+            let actualFramesPerSecond = deltaTime > 0 ? 1 / deltaTime : 60
+            
+            DispatchQueue.main.async {
+                self.tickHandler(actualFramesPerSecond)
+            }
+        }
+        
+        previousTimestamp = timestamp
+    }
+}
+
+extension CVTimeStamp {
+    var timeInterval: CFTimeInterval {
+        Double(videoTime) / Double(videoTimeScale)
+    }
+}
+
+#else
 public class DisplayLink: ObservableObject {
     private var tickHandler: (_ framesPerSecond: CFTimeInterval) -> Void = {_ in}
     private lazy var link = CADisplayLink(target: self, selector: #selector(tick))
@@ -22,3 +83,4 @@ public class DisplayLink: ObservableObject {
         tickHandler(actualFramesPerSecond)
     }
 }
+#endif
